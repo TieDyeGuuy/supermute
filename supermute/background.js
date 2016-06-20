258e62ab787b1c0fed032261d4fdafba6d4f5a10
Ben! <benpolson.zena@gmail.com>
2016-06-20 13:39:02 -0500

    Missed corrections

--- supermute/background.js
+++ supermute/background.js
@@ -1,6 +1,11 @@
+var hatedata
+
+// code by Kryptonite Dove
+// discovered in codepen.
+// https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
 function loadJSON(callback) {
   var xobj = new XMLHttpRequest();
-      xobj.overrideMimeType("application/json");
+  xobj.overrideMimeType("application/json");
   xobj.open('GET', 'web_source\\hbu.json', true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
@@ -8,4 +13,10 @@ function loadJSON(callback) {
     }
   };
   xobj.send(null);
+}
+
+function getHatedata() {
+  loadJSON(function(response) {
+    hatedata = JSON.parse(response);
+  });
 }
