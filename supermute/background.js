3b8f696b5f7e4777d547ba88b528d4a0844489d4
Ben! <benpolson.zena@gmail.com>
2016-06-22 17:32:05 -0500

    load data from hbu.json into app.

--- supermute/background.js
+++ supermute/background.js
@@ -1,22 +1,15 @@
-var hatedata
+var hateURL = chrome.extension.getURL("web_source/hbu.json");
+var hate = null;
 
-// code by Kryptonite Dove
-// discovered in codepen.
-// https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
-function loadJSON(callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'web_source\\hbu.json', true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
+console.log(hateURL);
+
+function transferComplete() {
+  hate = JSON.parse(this.responseText);
+  console.log(hate.status);
 }
 
-function getHatedata() {
-  loadJSON(function(response) {
-    hatedata = JSON.parse(response);
-  });
-}
+var xobj = new XMLHttpRequest();
+xobj.addEventListener("load", transferComplete)
+xobj.overrideMimeType("application/json");
+xobj.open('GET', hateURL);
+xobj.send();
