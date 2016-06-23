51eec5c21b8b18651837793491378b05ec0ee908
Ben! <benpolson.zena@gmail.com>
2016-06-22 23:38:27 -0500

    testing out manipulating data.

--- supermute/background.js
+++ supermute/background.js
@@ -1,15 +1,38 @@
 var hateURL = chrome.extension.getURL("web_source/hbu.json");
 var hate = null;
 
-console.log(hateURL);
-
 function transferComplete() {
   hate = JSON.parse(this.responseText);
-  console.log(hate.status);
+  var a = hate.data.datapoint.filter(generateSorter({
+    "offensiveness": 0.7
+  }));
+  a = a.map(function(item) {
+    return item.vocabulary;
+  });
+  console.log(a.toString());
 }
 
 var xobj = new XMLHttpRequest();
-xobj.addEventListener("load", transferComplete)
+xobj.addEventListener("load", transferComplete);
 xobj.overrideMimeType("application/json");
 xobj.open('GET', hateURL);
 xobj.send();
+
+function generateSorter(filters) {
+  return function(item) {
+    for(var prop in filters) {
+      switch(prop) {
+        case "offensiveness":
+          if(item[prop] < filters[prop]) {
+            return false;
+          }
+          break;
+        default:
+          if(!(item[prop] === filters[prop])) {
+            return false;
+          }
+      }
+    }
+    return true;
+  };
+}
