6f6b7ea14d50c7f935f7ce9cd80e0fa5d682767b
Ben! <benpolson.zena@gmail.com>
2016-06-30 17:00:51 -0500

    overhaul based on posnet's xkcd addon.

--- supermute/background.js
+++ supermute/background.js
@@ -1,70 +1,23 @@
-/* Table of contents:
- * 1. Upload section
- *   a. globals
- *   b. bang()
- *   c. start()
- *   d. uploadHate()
- *   e. localHateGet()
- *   f. transferComplete()
- *   g. transferFailed()
- *   h. transferCanceled()
- *   i. storeHate()
- *   j. restart()
- *   k. interpretStatus()
- *   l. uploadSuccess()
- * 2. Content Scripts Connection
- * 999. do stuff*/
-
-/*
- *------------------------------------------------------------------------------
- * 1. Upload section.
- *------------------------------------------------------------------------------
- */
-
-// a. globals
-var ON_HAND = 1;
-var KNOW_LOCAL = 2;
-var IS_LOCAL = 4;
-var UPLOAD = 8;
-var ABORT = 16;
-var RESTART = 32;
-var hate = null;
-var hatestatus = 0;
-
-/* 1  - is it on hand
- * 2  - do we know if it is locally stored?
- * 4  - is it locally stored?
- * 8  - was upload successful?
- * 16 - was upload aborted?
- * 32 - have we tried restarting?*/
+var debug = true;
 
 // b. bang()
 function bang() {
-  chrome.storage.local.get("hatestatus", start);
+  chrome.storage.local.get(null, start);
 }
 
 // c. start()
 /* This should be what happens first when the addon starts.*/
-function start(status) {
-  if(chrome.runtime.lastError) { // if we had trouble getting the status.
-    console.log(chrome.runtime.lastError);
-    hatestatus = hatestatus | RESTART; // assume we restarted.
-  } else if(status.hasOwnProperty("hatestatus")) { // if status is local.
-    hatestatus = status.hatestatus; // get most up to date status.
-  } else {
-    chrome.storage.local.set({"hatestatus": hatestatus});
-  }
-  if(hatestatus & ON_HAND === ON_HAND) {
-    if(hate == null) {
-      hatestatus -= ON_HAND;
-    } else {
-      return;
-    }
-  }
-  if(hatestatus & (KNOW_LOCAL + IS_LOCAL) === KNOW_LOCAL) {
+function start(result) {
+  if (!result["hatedata"]) {
     uploadHate();
   } else {
-    chrome.storage.local.get("hatedata", localHateGet);
+    try {
+      var hate = result.hatedata;
+      if (debug) {console.log("data already here");}
+      uploadSuccess();
+    } catch (e) {
+      uploadHate();
+    }
   }
 }
 
@@ -75,150 +28,24 @@ function uploadHate() {
   var hateURL = chrome.extension.getURL("web_source/hbu.json");
   var xobj = new XMLHttpRequest();
   xobj.addEventListener("load", transferComplete);
-  xobj.addEventListener("error", transferFailed);
-  xobj.addEventListener("abort", transferCanceled);
   xobj.overrideMimeType("application/json");
   xobj.open('GET', hateURL);
   xobj.send();
-}
-
-// e. localHateGet()
-/* This function should only be called after an attempt to retrieve hatedata
- * locally. If the retrieval is unsuccessful for any reason it tries to
- * upload the content from the addon data. If the retrieval is successful then
- * nothing more need be done.*/
-function localHateGet(data) {
-  if(chrome.runtime.lastError) {
-    console.log(chrome.runtime.lastError);
-    uploadHate();
-  } else {
-    var local = null;
-    try {
-      local = data.hasOwnProperty("hatedata");
-    } catch (e) {
-      local = false;
-    }
-    if(local) {
-      hate = data.hatedata;
-      hatestatus = hatestatus | (ON_HAND + KNOW_LOCAL + IS_LOCAL);
-      hatestatus = hatestatus & ~RESTART;
-      chrome.storage.local.set({"hatestatus": hatestatus});
-      uploadSuccess();
-    } else {
-      hatestatus = hatestatus | KNOW_LOCAL;
-      hatestatus = hatestatus & ~IS_LOCAL;
-      uploadHate();
-    }
-  }
 }
 
 // f. transferComplete()
 /* This function is called if an upload from the addon data is successful.
  * If hatedata is not stored locally then it attempts to store it locally.*/
 function transferComplete() {
-  hate = JSON.parse(this.responseText);
-  hatestatus = hatestatus | (ON_HAND + UPLOAD);
-  hatestatus = hatestatus & ~(ABORT + RESTART);
-  chrome.storage.local.set({"hatestatus": hatestatus});
+  if (debug) {console.log("load successful");}
+  var hate = JSON.parse(this.responseText);
+  chrome.storage.local.set({"hatedata": hate});
   uploadSuccess();
-  if((hatestatus & (KNOW_LOCAL + IS_LOCAL)) != (KNOW_LOCAL + IS_LOCAL)) {
-    chrome.storage.local.set({"hatedata": hate}, storeHate);
-  }
-}
-
-// g. transferFailed()
-/* This function is called if an upload from the addon data fails. If a reload
- * has not been attempted yet, then the function attempts a reload. If a reload
- * has been attempted then an error is thrown.*/
-function transferFailed() {
-  hatestatus = hatestatus & ~(UPLOAD + ABORT);
-  if(hatestatus & RESTART === 0) {
-    hatestatus += RESTART;
-    chrome.storage.local.set({"hatestatus": hatestatus}, restart);
-  } else {
-    var message = "Unable to load hatedata. Status: " + interpretStatus();
-    throw new Error(message);
-  }
-}
-
-// h. transferCanceled()
-/* This function is called if an upload from the addon data is canceled by the
- * user somehow. If a reload has not been attempted yet, then the function
- * attempts a reload. If a reload has been attempted then an error is thrown.*/
-function transferCanceled() {
-  hatestatus = hatestatus | ABORT;
-  hatestatus = hatestatus & ~UPLOAD;
-  if(hatestatus & RESTART === 0) {
-    hatestatus += RESTART;
-    chrome.storage.local.set({"hatestatus": hatestatus}, restart);
-  } else {
-    var message = "Unable to load hatedata. Status: " + interpretStatus();
-    throw new Error(message);
-  }
-}
-
-// i. storeHate()
-/* This function is called after a successful upload of the hatebase from the
- * addon data. Additionally it is intended to only be called if it is known
- * hatedata is not stored locally. It stores hate locally*/
-function storeHate() {
-  if(chrome.runtime.lastError) {
-    console.log(chrome.runtime.lastError);
-  } else {
-    hatestatus = hatestatus | (KNOW_LOCAL + IS_LOCAL);
-    hatestatus = hatestatus & ~RESTART;
-    console.log("status: " + interpretStatus());
-    chrome.storage.local.set({"hatestatus": hatestatus});
-  }
-}
-
-// j. restart()
-/* This function should only be called when attempting to reload the addon.
- * If there is an error storing hatestatus then an error is thrown.
- * If there is no error storing hatestatus then the reload occurs.*/
-function restart() {
-  if(chrome.runtime.lastError) {
-    console.log(chrome.runtime.lastError);
-    var message = "Unable to load hatedata. Status: " + interpretStatus();
-    throw new Error(message);
-  } else {
-    chrome.runtime.reload();
-  }
-}
-
-// k. interpretStatus()
-/* interprets the current hatestatus flags in an understandable way.*/
-function interpretStatus(status = hatestatus) {
-  var m = "";
-  if(status & ON_HAND) {
-    m += "hatedata is on hand, ";
-  }
-  if(!(status & KNOW_LOCAL)) {
-    m += "whether or not hatedata is local is unknown, ";
-  } else if (status & IS_LOCAL) {
-    m += "hatedata is stored locally, ";
-  } else {
-    m += "hatedata is not stored locally, ";
-  }
-  if(status & UPLOAD) {
-    m += "hatedata was uploaded, ";
-  } else if(status & ABORT) {
-    m += "hatedata upload was aborted, ";
-  } else {
-    m += "hatedata upload failed, ";
-  }
-  if(status & RESTART) {
-    m += "a restart was attempted.";
-  } else {
-    m += "no restart.";
-  }
-  return m;
 }
 
 // l. uploadSuccess()
-/* */
 function uploadSuccess() {
-  connectStart();
+  //connectStart();
 }
 
 /*
@@ -233,17 +60,13 @@ function uploadSuccess() {
  *------------------------------------------------------------------------------
  */
 
-var topWords = null;
 var ports = {};
 
 /* Begin organizing data and talking with any active web pages.*/
 function connectStart() {
-  topWords = hate.data.datapoint.filter(generateSorter({"offensiveness": 0.5}));
   chrome.browserAction.onClicked.addListener(bclickListen);
   chrome.runtime.onMessage.addListener(mainListener);
-  chrome.tabs.onRemoved.addListener(removeListener);
   chrome.tabs.onUpdated.addListener(updateListener);
-  chrome.tabs.onActivated.addListener(activeListener);
   chrome.tabs.query({}, insertScript);
 }
 
@@ -253,9 +76,6 @@ function bclickListen(tab) {
 
 /* This is the main listener of the addon.*/
 function mainListener(message, sender, sendResponse) {
-  if (message === null || typeof message !== 'object') {
-    throw new Error("Malicious message");
-  }
   if(message.hasOwnProperty("request")) {
     switch(message.request) {
       case "connect":
@@ -268,38 +88,14 @@ function mainListener(message, sender, sendResponse) {
   }
 }
 
-function removeListener(tabId, removeInfo) {
-  if (ports.hasOwnProperty(tabId.toString())) {
-    ports[tabId.toString()].disconnect();
-    delete ports[tabId.toString()];
-  }
-}
-
 function updateListener(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    if (!ports.hasOwnProperty(tabId.toString())) {
-      addScript(tabId);
-    } else {
-      chrome.tabs.query({active: true, currentWindow: true},
-                        function (result) {
-            if (result[0].id === tabId) {
-              ports[message.port].postMessage({request: "search",
-                                               words: topWords});
-            }
-          });
-    }
-  }
-}
-
-function activeListener(activeInfo) {
-  if (ports.hasOwnProperty(activeInfo.tabId.toString())) {
-    ports[activeInfo.tabId.toString()].postMessage({request: "search",
-                                                    words: topWords});
-  }
+  if (debug) {console.log("injection fire");}
+  addScript(tabId);
 }
 
 /* Function to insert script into each page.*/
 function insertScript(tbs) {
+  if (debug) {console.log("first insert");}
   for(let t of tbs) {
     addScript(t.id);
   }
@@ -328,8 +124,12 @@ function portListener(message) {
 }
 
 function addScript(tabId) {
-  chrome.tabs.executeScript(tabId,
-                            {file: "content_script.js"});
+  chrome.tabs.executeScript(tabId, {
+    file: "content_script.js",
+    runAt: "document_end"
+  }, function () {
+    if (debug) {console.log("script executed");}
+  });
 }
 
 /*
