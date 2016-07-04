324d69d1de0e69df6ab0ad77611c4e8320b491f2
Ben! <benpolson.zena@gmail.com>
2016-07-04 15:30:02 -0500

    overhaul attempt

--- supermute/background.js
+++ supermute/background.js
@@ -1,3 +1,7 @@
+/* new strategy based on:
+ * https://github.com/Posnet/xkcd-substitutions
+ */
+
 var debug = true;
 
 // b. bang()
@@ -13,7 +17,10 @@ function start(result) {
   } else {
     try {
       var hate = result.hatedata;
-      if (debug) {console.log("data already here");}
+      if (debug) {
+        console.log("status: " + hate.status);
+        console.log("data already here");
+      }
       uploadSuccess();
     } catch (e) {
       uploadHate();
@@ -45,14 +52,8 @@ function transferComplete() {
 
 // l. uploadSuccess()
 function uploadSuccess() {
-  //connectStart();
+  connectStart();
 }
-
-/*
- *------------------------------------------------------------------------------
- * End upload section.
- *------------------------------------------------------------------------------
- */
 
 /*
  *------------------------------------------------------------------------------
@@ -67,7 +68,6 @@ function connectStart() {
   chrome.browserAction.onClicked.addListener(bclickListen);
   chrome.runtime.onMessage.addListener(mainListener);
   chrome.tabs.onUpdated.addListener(updateListener);
-  chrome.tabs.query({}, insertScript);
 }
 
 function bclickListen(tab) {
@@ -76,54 +76,25 @@ function bclickListen(tab) {
 
 /* This is the main listener of the addon.*/
 function mainListener(message, sender, sendResponse) {
-  if(message.hasOwnProperty("request")) {
-    switch(message.request) {
-      case "connect":
-        var id = sender.tab.id;
-        var p = chrome.tabs.connect(id, {name: id.toString()});
-        ports[id.toString()] = p;
-        p.onMessage.addListener(portListener);
-        break;
+  if (debug) {console.log("message fire");}
+  chrome.storage.local.get(null, function (result) {
+    if (message["request"]
+        && message["request"] === "words"
+        && result["hatedata"]) {
+      var words = result["hatedata"].data.datapoint.filter(
+          generateSorter(message["filters"]));
+      if (debug) {
+        console.log("words: " + words.map(function(item) {
+          return item.vocabulary;
+        }).toString());
+      }
+      //sendResponse(words)
     }
-  }
+  });
 }
 
 function updateListener(tabId, changeInfo, tab) {
-  if (debug) {console.log("injection fire");}
-  addScript(tabId);
-}
-
-/* Function to insert script into each page.*/
-function insertScript(tbs) {
-  if (debug) {console.log("first insert");}
-  for(let t of tbs) {
-    addScript(t.id);
-  }
-}
-
-function portListener(message) {
-  if (message === null || typeof message !== 'object') {
-    throw new Error("Malicious message");
-  }
-  if(message.hasOwnProperty("request")) {
-    switch(message.request) {
-      case "get words":
-        ports[message.port].postMessage({response: "words", words: topWords});
-        break;
-      case "ready":
-        chrome.tabs.query({active: true, currentWindow: true},
-                          function (result) {
-              if (result[0].id.toString() === message.port) {
-                ports[message.port].postMessage({request: "search",
-                                                 words: topWords});
-              }
-            });
-        break;
-    }
-  }
-}
-
-function addScript(tabId) {
+  if (debug) {console.log(tabId + " " + tab.status + " injection fire");}
   chrome.tabs.executeScript(tabId, {
     file: "content_script.js",
     runAt: "document_end"
@@ -138,47 +109,60 @@ function addScript(tabId) {
  *------------------------------------------------------------------------------
  */
 
- /* as of right now filters are any property of a datapoint in hbu.json
-  * the relevant possibilities are as follows:
-  * about_class, about_disability, about_ethnicity, about_gender,
-  * about_nationality, about_religion, about_sexual_orientation, archaic,
-  * number_of_variants, offensiveness, other_meaning, and small_acronym.
-  *
-  * support for the following filters will come soon:
-  * primary_contexts, primary_location, variant_of
-  *
-  * the parameter filters should be an object where each key is one of the
-  * property names listed above and each value is the desired value.
-  *
-  * the function returns a filter function so that it is easy to filter the
-  * datapoint array. More explicitly if you check out this page:
-  * http://www.w3schools.com/jsref/jsref_filter.asp
-  * then the result of this function is what you would pass to the filter
-  * function for example:
-  * hate.data.datapoint.filter(generateSorter({"offensiveness": .5,
-                                               "about_class": "1"}));
-  * would return an array of all words in the hatebase with an offensiveness
-  * greater than or equal to .5 and are about class.*/
- function generateSorter(filters) {
-   return function(item) {
-     for(var prop in filters) {
-       switch(prop) {
-         // TODO create cases for primary_contexts, primary_location
-         // and variant_of.
-         case "offensiveness":
-           if(item[prop] < filters[prop]) {
-             return false;
-           }
-           break;
-         default:
-           if(!(item[prop] === filters[prop])) {
-             return false;
-           }
-       }
-     }
-     return true;
-   };
- }
+/* as of right now filters are any property of a datapoint in hbu.json
+ * the relevant possibilities are as follows:
+ * about_class, about_disability, about_ethnicity, about_gender,
+ * about_nationality, about_religion, about_sexual_orientation, archaic,
+ * number_of_variants, offensiveness, other_meaning, and small_acronym.
+ *
+ * support for the following filters will come soon:
+ * primary_contexts, primary_location, variant_of
+ *
+ * the parameter filters should be an object where each key is one of the
+ * property names listed above and each value is the desired value.
+ *
+ * the function returns a filter function so that it is easy to filter the
+ * datapoint array. More explicitly if you check out this page:
+ * http://www.w3schools.com/jsref/jsref_filter.asp
+ * then the result of this function is what you would pass to the filter
+ * function for example:
+ * hate.data.datapoint.filter(generateSorter({"offensiveness": .5,
+                                              "about_class": "1"}));
+ * would return an array of all words in the hatebase with an offensiveness
+ * greater than or equal to .5 and are about class.*/
+function generateSorter(filters) {
+  return function(item) {
+    for(var prop in filters) {
+      switch(prop) {
+        // TODO create cases for primary_contexts, primary_location
+        // and variant_of.
+        case "offensiveness":
+          if(item[prop] < filters[prop]) {
+            return false;
+          }
+          break;
+        case "starts_with":
+          if (!item.vocabulary.toLowerCase().startsWith(
+              filters[prop].toLowerCase())) {
+            return false;
+          }
+          break;
+        case "mild":
+          if (filters[prop] && item.offensiveness > 0.0) {
+            return false;
+          } else if (!filters[prop] && item.offensiveness === 0.0) {
+            return false;
+          }
+          break;
+        default:
+          if(!(item[prop] === filters[prop])) {
+            return false;
+          }
+      }
+    }
+    return true;
+  };
+}
 
 // 999. do stuff
 bang();
--- supermute/content_script.js
+++ supermute/content_script.js
@@ -1,39 +1,12 @@
-var port = null;
+var debug = true;
 
-function connectListener(p) {
-  port = p;
-  p.onMessage.addListener(portListener);
-  p.postMessage({request: "ready", port: p.name});
-}
+if (debug) {console.log("debug");}
 
-function portListener(message) {
-  if (message === null || typeof message !== 'object') {
-    throw new Error("Malicious message");
+chrome.runtime.sendMessage({
+  "request": "words",
+  "filters": {
+    "mild": "1",
+    "starts_with": "ap",
+    "about_ethnicity": "1"
   }
-  if (message.hasOwnProperty("response")) {
-    switch(message.response) {
-      case "words":
-        blockWord("Yes");
-        break;
-    }
-  }
-  if (message.hasOwnProperty("request")) {
-    switch (message.request) {
-      case "search":
-        blockWord(message.words);
-        break;
-    }
-  }
-}
-
-function blockWord(topWords) {
-  var rword = topWords[getRandomInt(0, topWords.length)].vocabulary;
-  console.log(port.name + " " + rword);
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-chrome.runtime.onConnect.addListener(connectListener);
-chrome.runtime.sendMessage({"request": "connect"});
+});
