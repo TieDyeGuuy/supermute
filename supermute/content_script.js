7308dedda674ec1e450dedf833b172e3c701f596
Ben! <benpolson.zena@gmail.com>
2016-07-04 23:34:13 -0500

    filter out empty and nonvisible nodes

--- supermute/content_script.js
+++ supermute/content_script.js
@@ -6,11 +6,30 @@ function test() {
 
 function getID(message) {
   var node, iter;
-  iter = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+  iter = document.createNodeIterator(
+      document.body,
+      NodeFilter.SHOW_TEXT,
+      function (node) {
+        var ignore = {
+          "STYLE": 0,
+          "SCRIPT": 0,
+          "NOSCRIPT": 0,
+          "IFRAME": 0,
+          "OBJECT": 0,
+          "INPUT": 0,
+          "FORM": 0,
+          "TEXTAREA": 0
+        };
+        if (node.parentElement.tagName in ignore) {
+          return NodeFilter.FILTER_REJECT;
+        }
+        return node.textContent.trim() !== "" ?
+            NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
+      });
   while ((node = iter.nextNode())) {
-    console.log(node.parentElement.id)
+    console.log(node.parentElement.id);
   }
 }
 
 test();
-chrome.runtime.onMessage.addListener(test);
+chrome.runtime.onMessage.addListener(getID);
